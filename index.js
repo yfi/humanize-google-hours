b5834a6383ee6af426563476ba90a033254db15d
Yarek Waszul <yarek@yarekwaszul.com>
2022-11-25 13:21:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,46 @@
-exports.humanizeHours = function(periods) {
-    let lines = reduceLines(periods);
-    return lines.map((el) => humanizeLine(el));
-};
+    import dayjs from 'dayjs';
+    import customParseFormat from 'dayjs/plugin/customParseFormat';
+    dayjs.extend(customParseFormat);
+    import get from 'just-safe-get';
 
-function reduceLines(periods) {
-    if (!periods) return false;
-    let lines = [];
-    let line = Object.assign(periods[0]);
-    periods.forEach((el, i, arr) => {
-        if (i > 0) {
-            let prevEl = arr[i - 1];
+    exports.humanizeHours = (periods) => {
+
+        if (periods[0].open.day==0) periods.push(periods.shift());
+
+        let lines = periods.reduce((acc,cur,i)=>{
+        
+            const isLast = i == periods.length-1;
+            let prev = get(acc, (acc.length-1).toString());
+
+            if (!prev) return [cur];
+            
             if (
-                el.open.day - 1 != prevEl.open.day ||
-                el.open.time != line.open.time ||
-                el.close.time != line.close.time ||
-                i == arr.length - 1
+                get(prev ,'open.time')!=cur.open.time ||
+                get(prev ,'close.time')!=cur.close.time 
             ) {
-                line.close.day = prevEl.close.day;
-                lines.push(line);
-                line = Object.assign(el);
+            
+                if ( cur.open.day - prev.open.day > Math.abs(1) ) {
+                    (prev.close.day = (+cur.close.day-1).toString());
+                } 
+                
+                return [...acc,cur];
+            
             }
-        }
-    });
+
+            if (isLast) prev.close.day = cur.close.day;  
+            return acc;
+
+        },[]);
+
+        lines = lines.reduce((acc,cur)=>{
+
+            let days = [formatDay(cur.open.day)];
+            if ( cur.open.day != cur.close.day ) days.push(formatDay( cur.close.day ));
+     
+            let hours = [formatHour(cur.open.time),formatHour(cur.close.time)];
+            const line = {days,hours};
+            return [...acc, line];
+
+        },[]);
+
     return lines;
-}
-
-function humanizeLine(period) {
-    const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-    let line = days[period.open.day];
-    if (period.close.day != period.open.day)
-        line += "&mdash;" + days[period.close.day];
-    line +=
-        " " + toAmPm(period.open.time) + "&mdash;" + toAmPm(period.close.time);
-    return line;
-};
-
-function toAmPm(hhmm) {
-    const meridien = hhmm.substring(0, 2) >= 12 ? "PM" : "AM";
-    const hours = hhmm.substring(0, 2) % 12 || 12;
-    const minutes = eval(hhmm.substring(2, 2));
-
-    let time = hours;
-    if (minutes) time += ":" + minutes;
-    time += meridien;
-    return time;
-};
-
-function simplifyLine(line) {
-    line = line.replace(/:|00| /g, "");
-    line = line.replace(
-        /Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday/gi,
-        function(match) {
-            return match.substring(0, 1).toUpperCase() + " ";
-        }
-    );
-    line = line.replace(/closed/i, "&mdash;");
-
-    return line;
-};
